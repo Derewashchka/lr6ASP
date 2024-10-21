f1ba9298c0cd4fbe8ef2bad6595acfaecba5375f
My comp <My comp@DESKTOP-HBDEDEI>
2024-10-21 17:35:59 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(User user)
+    {
+        if (user.IsOldEnough())
+        {
+            return RedirectToAction("Order", new { userName = user.Name });
+        }
+        else
+        {
+            ViewBag.Message = "Вам повинно бути більше 16 років для замовлення.";
+            return View();
+        }
+    }
+
+    [HttpGet]
+    public IActionResult Order(string userName)
+    {
+        ViewBag.UserName = userName;
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Order(int productCount)
+    {
+        return RedirectToAction("ProductForm", new { count = productCount });
+    }
+
+    [HttpGet]
+    public IActionResult ProductForm(int count)
+    {
+        ViewBag.Count = count;
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult ProductForm(List<Product> products)
+    {
+        return View("Summary", products);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿public class User
+{
+    public string Name { get; set; }
+    public DateTime BirthDate { get; set; }
+
+    public bool IsOldEnough()
+    {
+        return (DateTime.Now.Year - BirthDate.Year) >= 16;
+    }
+}
